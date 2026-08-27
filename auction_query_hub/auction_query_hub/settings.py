8f29974c8f15943155cb14bf6fde9afdec8bf32d
Dion James Smith <dion.smith@live.com>
2026-08-27 10:29:47 +0530

    Add localhost to ALLOWED_HOSTS in settings.py

--- auction_query_hub/auction_query_hub/settings.py
+++ auction_query_hub/auction_query_hub/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
-ALLOWED_HOSTS = ['192.168.0.100']
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.0.100']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
